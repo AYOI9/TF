db22187915d3249499e4136862eb6b419e4acbc8
maior <maior@DESKTOP-FHGLER9>
2024-12-27 02:47:53 +0200

    Добавьте файлы проекта.

---
+++ TF/Program.cs
@@ -0,0 +1,16 @@
+﻿
+    
+        double start = 0.0, end = 3.0, step = 0.2;
+
+        Console.WriteLine("  x\t\t  y");
+        Console.WriteLine("-----------------------");
+
+        for (double x = start; x <= end; x += step)
+        {
+            double y = Math.Pow(x, 2) + 4 * Math.Sin(Math.PI * x);
+            Console.WriteLine($"{x:F1}\t{y:F4}");
+        }
+
+        Console.ReadKey();
+    
+
